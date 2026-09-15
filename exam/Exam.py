45029b916a172c5d3dd66e61c71adc2cb834e5ba
Christian Kyle J. Wong <2302702@cclab.usls.edu>
2026-09-15 15:57:59 +0800

    exam.py

---
+++ exam/Exam.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+
+class student (Exception)
+    def __init__  (self name, scores)
+ self.Name = Name
+ self.Scores = scores
+    
+
+    score = (75, 85,)
+def __str__(self):
+        return f"{self.name} ({self.scores}
